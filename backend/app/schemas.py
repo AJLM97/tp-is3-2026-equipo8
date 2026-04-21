8a7c30d7fb3b53b034f06e2a1c686610d64c708c
AJLM97 <AJLM97@users.noreply.github.com>
2026-04-21 14:53:32 -0300

    feat: implementar validación de formato y extracción de ZIP (WBS 1.1.3)
    Se integró validación en FastAPI para restringir archivos a .txt y .zip (Error 400).
    Se desarrolló la lógica para leer archivos comprimidos y el parseo inicial de mensajes.
    
    Co-authored-by: Thomy98 <Thomy98@users.noreply.github.com>

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,4 +1,12 @@
 from pydantic import BaseModel
+from typing import List
+
+class Message(BaseModel):
+    date: str
+    hour: str
+    user: str
+    message: str
 
 class ChatResponse(BaseModel):
     chat_name: str
+    messages: List[Message]
